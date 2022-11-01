031ed50f4adaabec26ac47a382998a61b860699b
Max Gonzih <gonzih@gmail.com>
2022-10-31 19:58:23 -0700

    Format

--- main.py
+++ main.py
@@ -5,28 +5,29 @@ from logging import info
 coloredlogs.install(level='DEBUG')
 
 movement_map = {
-  "d": "h",
-  "h": "j",
-  "t": "k",
-  "n": "l",
+    "d": "h",
+    "h": "j",
+    "t": "k",
+    "n": "l",
 }
 
 generic_map = {
-  "l": "n",
-  "j": "d",
-  ";": ":",
-  "-": "$",
+    "l": "n",
+    "j": "d",
+    ";": ":",
+    "-": "$",
 }
 
 insert_map = {
-  "<C-c>": "<Esc>",
+    "<C-c>": "<Esc>",
 }
 
 movement_keys = [
-  "vim.commandLineModeKeyBindingsNonRecursive",
-  "vim.normalModeKeyBindingsNonRecursive",
-  "vim.visualModeKeyBindingsNonRecursive",
+    "vim.commandLineModeKeyBindingsNonRecursive",
+    "vim.normalModeKeyBindingsNonRecursive",
+    "vim.visualModeKeyBindingsNonRecursive",
 ]
+
 
 def with_prefix(prefix: str, pair):
     if not prefix:
@@ -39,6 +40,7 @@ def with_prefix(prefix: str, pair):
 
     return newpair
 
+
 def to_upper(pair):
     newpair = {}
 
@@ -46,6 +48,7 @@ def to_upper(pair):
         newpair[k] = list(map(lambda v: v.upper(), pair[k]))
 
     return newpair
+
 
 def gen_map(keymap, cfgkeys, add_upper_case: bool = False, prefix: str = ""):
     settings = {}
@@ -55,12 +58,13 @@ def gen_map(keymap, cfgkeys, add_upper_case: bool = False, prefix: str = ""):
 
         for before in keymap:
             after = keymap[before]
-            pair = { "before": [before], "after": [after] }
+            pair = {"before": [before], "after": [after]}
             settings[k].append(with_prefix(prefix, pair))
             if add_upper_case:
                 settings[k].append(with_prefix(prefix, to_upper(pair)))
 
     return settings
+
 
 def generate():
     overlap_key = "vim.normalModeKeyBindingsNonRecursive"
@@ -72,8 +76,10 @@ def generate():
 
     return insert | all
 
+
 template_json = "template.json"
 settings_json = "settings.json"
+
 
 def run():
     info(f"Reading {template_json}")
@@ -86,6 +92,7 @@ def run():
     info(f"Writing {settings_json}")
     with open(settings_json, "w") as f:
         json.dump(settings, f, indent=2)
+
 
 if __name__ == "__main__":
     run()
