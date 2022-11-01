d369ed02ac6a9e0de66f46ea571758ae1aa48d07
Max Gonzih <gonzih@gmail.com>
2022-10-31 19:44:55 -0700

    Trying pyhon thing

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 run:
-	deno run --allow-read --allow-write main.ts --source template.json --output settings.json
+	poetry run python main.py
+
+mac-install:
+	cp -f settings.json /Users/gnzh/Library/Application Support/Code/User/settings.json
 
 wsl-install:
 	cp -f settings.json /mnt/c/Users/gnzh/AppData/Roaming/Code/User/settings.json
---
+++ main.py
@@ -0,0 +1,90 @@
+import json
+import coloredlogs
+from logging import info
+
+coloredlogs.install(level='DEBUG')
+
+movement_map = {
+  "d": "h",
+  "h": "j",
+  "t": "k",
+  "n": "l",
+}
+
+generic_map = {
+  "l": "n",
+  "j": "d",
+  ";": ":",
+  "-": "$",
+}
+
+insert_map = {
+  "<C-c>": "<Esc>",
+}
+
+movement_keys = [
+  "vim.commandLineModeKeyBindingsNonRecursive",
+  "vim.normalModeKeyBindingsNonRecursive",
+  "vim.visualModeKeyBindingsNonRecursive",
+]
+
+def with_prefix(prefix: str, pair):
+    newpair = {}
+    for k in pair:
+        newpair[k] = [prefix]
+        newpair[k].append(pair[k])
+
+    return newpair
+
+def to_upper(pair):
+    newpair = {}
+
+    for k in pair:
+        newpair[k] = list(map(lambda v: v.upper(), pair[k]))
+
+    return newpair
+
+def gen_map(keymap, cfgkeys, add_upper_case: bool = False, prefix: str = ""):
+    settings = {}
+    for k in cfgkeys:
+        if k not in settings:
+            settings[k] = []
+
+        for before in keymap:
+            after = keymap[before]
+            pair = { "before": [before], "after": [after] }
+            if prefix:
+                settings[k].append(with_prefix(prefix, pair))
+                settings[k].append(with_prefix(prefix, to_upper(pair)))
+            else:
+                settings[k].append(pair)
+                if add_upper_case:
+                    settings[k].append(to_upper(pair))
+
+    return settings
+
+def generate():
+    insert = gen_map(insert_map, ["vim.insertModeKeyBindingsNonRecursive"])
+    all = gen_map(generic_map | movement_map, movement_keys, True)
+    movement = gen_map(movement_map, ["vim.normalModeKeyBindingsNonRecursive"], True, "<C-w>")
+
+    return insert | all | movement
+
+template_json = "template.json"
+settings_json = "settings.json"
+
+def run():
+    info(f"Reading {template_json}")
+    with open(template_json, "r") as f:
+        template = json.load(f)
+
+    settings = template | generate()
+    print(settings)
+    print(json.dumps(settings, indent=2))
+
+    info(f"Writing {settings_json}")
+    with open(settings_json, "w") as f:
+        json.dump(settings, f, indent=2)
+
+if __name__ == "__main__":
+    run()
